38973e8705de6a813ebc1b2c0d0057706c4fc35b
Emir Halıcı <emir.halici1@gmail.com>
2023-07-10 14:30:26 +0300

    feat: add home page contents

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,17 @@
+import { TextSubtitle, TextTitle } from '~/components/Texts/Texts';
 import './HomePage.css';
 
 export default function HomePage() {
-  return <div className='page-fixed-width-root'>Hello world!</div>;
+  return (
+    <div className='page-fixed-width-root'>
+      <TextTitle>Hello, I&apos;m Emir 👋</TextTitle>
+      <TextSubtitle>
+        I am a software developer based in <b>Türkiye</b>.
+      </TextSubtitle>
+      <p>
+        I am currently remotely freelancing and working on receiving my bachelor&apos;s (3/4) in{' '}
+        <b>Ankara</b>. Check out my work and socials down below!
+      </p>
+    </div>
+  );
 }
