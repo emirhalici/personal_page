3ca3349c91739882b032684ae1debc9682c9576c
Emir Halıcı <81600010+emirhalici@users.noreply.github.com>
2024-08-13 18:39:02 +0300

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -11,8 +11,7 @@ export default function HomePage() {
         I am a software developer based in <b>Türkiye</b>.
       </TextSubtitle>
       <p>
-        I am currently remotely freelancing and working on receiving my bachelor&apos;s (4/4) in{' '}
-        <b>Ankara</b>. Check out my socials down below or more learn more about me{' '}
+        Check out my socials down below or more learn more about me{' '}
         <Link to={routeNames.about.path}> here</Link>!
       </p>
       <SocialLinks />
